98f2a6497c930aedbdd381afe088264db9ed2952
Andreas Wolf <andreas.wolf@usta.de>
2014-04-30 14:47:39 +0200

    [FEATURE] User authentication

---
+++ Classes/AstaKit/FriWahl/AdminInterface/Controller/StandardController.php
@@ -0,0 +1,25 @@
+<?php
+namespace AstaKit\FriWahl\AdminInterface\Controller;
+
+/*                                                                                  *
+ * This script belongs to the TYPO3 Flow package "AstaKit.FriWahl.AdminInterface".  *
+ *                                                                                  *
+ *                                                                                  */
+
+use TYPO3\Flow\Mvc\Controller\ActionController;
+
+
+/**
+ * Default controller for the admin interface.
+ *
+ * @author Andreas Wolf <andreas.wolf@usta.de>
+ */
+class StandardController extends ActionController {
+
+	/**
+	 *
+	 */
+	public function indexAction() {
+
+	}
+}
